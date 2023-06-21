dfbf89fb34623c6cc2e85dc3d2cd112563078bfb
Liubov Gayduk <serenity0506@yandex.ru>
2023-06-21 11:52:04 +0300

    add cors

--- app.js
+++ app.js
@@ -13,6 +13,31 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
 
 app.use(helmet());
 
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req;
+
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+});
+
 app.use(bodyParser.json());
 
 const limiter = rateLimit({
